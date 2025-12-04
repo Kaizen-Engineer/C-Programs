9b5aff5526bd706e6b55e11a401fe1f474ff96ff
Kar Prabhakar <110916707+Lazerzap007@users.noreply.github.com>
2025-12-04 12:33:43 +0530

    Add project files.

---
+++ firstcode.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+	int a = 50;
+	int b = 100;
+
+	printf("Hello, Kaizen Engineering!! \n");
+
+	int sum = a + b;
+	printf("Sum of a and b is %d\n", sum);
+
+	int sub = a - b;
+	printf("Sub of a and b is %d\n", sub);
+
+	int prod = a * b;
+	printf("Product of a and b is %d\n", prod);
+
+	int div = a / b;
+	printf("Divison of a and b is %d\n", div);
+
+	return 0;
+}
